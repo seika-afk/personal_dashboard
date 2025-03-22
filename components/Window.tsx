9f286c8865eee5c34045dae392ad251e39b7025b
_Gagan <84296830+seika-afk@users.noreply.github.com>
2025-03-22 17:57:58 +0000

    Update Window.tsx

--- components/Window.tsx
+++ components/Window.tsx
@@ -12,7 +12,6 @@ import React, { useState, useRef } from "react";
 const Window: React.FC<WindowProps> = ({ title, children, defaultPosition, w, h }) => {
   const [position, setPosition] = useState({ top: defaultPosition.top, left: defaultPosition.left });
   const [size, setSize] = useState({ width: w, height: h });
-  const [isDragging, setDragging] = useState(false);
   const [highestZ, setHighestZ] = useState(1);
   const windowRef = useRef<HTMLDivElement>(null);
 
